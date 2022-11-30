a578d26b6d0a3bd17fc93909ab4a5549a1162a62
samik shk <samik.shakya8848@gmail.com>
2022-11-30 18:11:42 +0545

    registerandlogin

--- database/migrations/2022_11_30_101526_create_users_table.php
+++ database/migrations/2022_11_30_101526_create_users_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('authtable', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('remember_token')->nullable();
             $table->timestamps();
         });
     }
